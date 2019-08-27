f1a6408b21e427335be79e0552b84079dfc7b0b6
Karl Zander <pvial@Karls-MacBook-Air.local>
2019-08-27 12:23:13 -0700

    Ganja KDF

--- ganjakdf/ganja_kdf.c
+++ ganjakdf/ganja_kdf.c
@@ -8,6 +8,10 @@ int main(int argc, char *argv[]) {
     unsigned char D[32] = {0};
     unsigned char *buf;
     char digest[32*2+1];
+    if (argc != 2) {
+        printf("ganjakdf <password>\n");
+        exit(1);
+    }
     password = argv[1];
     ganja_kdf(password, strlen(password), D, iterations, keylen, salt);
     for (int x = 0; x < 32; x++) {
